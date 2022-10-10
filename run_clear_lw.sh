f32fe4b48b8b5221c032fa6b3d08948a44420075
Jing Feng <Jing.Feng@ldt-4359639.gfdl.noaa.gov>
2022-10-10 18:20:35 -0400

    add a test file

--- run_clear_lw.sh
+++ run_clear_lw.sh
@@ -6,6 +6,6 @@ module load gcc/11.2.0
 module load netcdf-c/4.8.1
 module load netcdf-fortran/4.5.3
 
-./clearsky_rrtmgp test.nc\
+./examples/rfmip-clear-sky/clearsky_rrtmgp test.nc\
                            ./rrtmgp/data/rrtmgp-data-lw-g256-2018-12-04.nc \
                            ./rrtmgp/data/rrtmgp-data-sw-g224-2018-12-04.nc
